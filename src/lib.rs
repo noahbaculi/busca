6d58936cd9dd965b85aab2c2526215284f6b891b
Noah <nbaculi@hotmail.com>
2023-04-22 12:42:03 -0700

    migrate to clap library

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,15 @@
-use core::panic;
 use std::error::Error;
-use std::fs;
+// use std::fs;
 use std::path::PathBuf;
 
-#[derive(Debug)]
-pub struct Config {
-    pub comparison_file_path: PathBuf,
-    pub search_dir: String,
-}
-
-impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() - 1 < 2 {
-            return Err("not enough arguments");
-        }
-
-        // The main comparison reference file is the first argument
-        let mut comparison_file_path = PathBuf::new();
-        comparison_file_path.push(&args[1]);
-        if !comparison_file_path.is_file() {
-            if !comparison_file_path.is_file() {
-                return Err("The comparison file does not exist");
-            }
-        }
-
-        // TODO make optional
-        let search_dir = args[2].clone();
-
-        Ok(Config {
-            comparison_file_path,
-            search_dir,
-        })
-    }
-}
-
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+pub fn search(comparison_file_path: PathBuf, search_dir: PathBuf) -> Result<(), Box<dyn Error>> {
     // let contents = fs::read_to_string(config.search_dir)?;
 
-    // println!("With text:\n{contents}");
+    println!(
+        "{} | {}",
+        comparison_file_path.display(),
+        search_dir.display()
+    );
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,69 @@
-use std::env;
-use std::process;
+use clap::Parser;
+use std::{env, path::PathBuf};
 
-use busca::Config;
+/// Simple utility to find the closest matches to a reference file in a
+/// directory based on the number of lines in the reference file that exist in
+/// each compared file.
+#[derive(Parser, Debug)]
+#[command(author="Noah Baculi", version, about, long_about = None)]
+struct InputArgs {
+    /// Local or absolute path to the reference comparison file
+    ref_file_path: PathBuf,
+
+    /// Directory or file in which to search. Defaults to CWD
+    #[arg(short, long)]
+    search_path: Option<PathBuf>,
+
+    /// Number of results to display
+    #[arg(short, long, default_value_t = 10)]
+    count: u8,
+}
+
+#[derive(Debug)]
+struct Args {
+    ref_file_path: PathBuf,
+    search_path: PathBuf,
+    count: u8,
+}
+
+fn validate_args(input_args: InputArgs) -> Args {
+    if !input_args.ref_file_path.is_file() {
+        panic!(
+            "The reference file path '{}' could not be found",
+            input_args.ref_file_path.display()
+        );
+    }
+
+    // Assign to CWD if the arg is not given
+    let search_path = input_args
+        .search_path
+        .clone()
+        .unwrap_or(env::current_dir().unwrap());
+
+    if !search_path.is_file() & !search_path.is_dir() {
+        panic!(
+            "The search path '{}' could not be found",
+            search_path.display()
+        );
+    }
+
+    let mut count = input_args.count;
+    if (count == 0) | (count > 200) {
+        count = 10
+    }
+
+    Args {
+        ref_file_path: input_args.ref_file_path,
+        search_path,
+        count,
+    }
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let input_args = InputArgs::parse();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
+    let args = validate_args(input_args);
 
-    dbg!(&config);
-
-    println!("Searching for {}", config.comparison_file_path.display());
-    println!("In file {}", config.search_dir);
-
-    if let Err(e) = busca::run(config) {
-        eprintln!("Application error: {e}");
-        process::exit(1);
-    }
+    dbg!(&args);
+    dbg!(&args.ref_file_path.display());
 }
