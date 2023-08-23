290b6473bcd0d4cf19d6cfd5992bde5fe233f71e
noahbaculi <nbaculi@carium.com>
2023-08-23 10:25:09 -0700

    working python module

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use glob::Pattern;
+use pyo3::prelude::*;
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use similar::{ChangeTag, TextDiff};
 use std::fmt;
@@ -6,6 +7,21 @@ use std::fs;
 use std::path::{Path, PathBuf};
 use term_grid::{Alignment, Cell, Direction, Filling, Grid, GridOptions};
 use walkdir::WalkDir;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+/// A Python module implemented in Rust. The name of this function must match
+/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
+/// import the module.
+#[pymodule]
+fn busca(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    Ok(())
+}
 
 #[derive(Debug, PartialEq)]
 pub struct Args {
