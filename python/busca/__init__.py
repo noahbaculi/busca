478d886705c890e1e25e09bfb951358e02247bfc
noahbaculi <nbaculi@carium.com>
2023-08-24 09:11:53 -0700

    working rust/python mixed project layout

---
+++ python/busca/__init__.py
@@ -0,0 +1,5 @@
+from .busca import *
+
+__doc__ = busca.__doc__
+if hasattr(busca, "__all__"):
+    __all__ = busca.__all__
---
+++ python/test.py
@@ -0,0 +1,6 @@
+import busca
+
+print(busca)
+
+x = busca.search_for_lines("Hi", "./", 10000, 20)
+print(x)
