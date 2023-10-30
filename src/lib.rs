ab1bcdc2dabed37bcd5b635f2376a1488e71214e
noahbaculi <nbaculi@carium.com>
2023-10-30 10:19:09 -0700

    fix items_after_test_module lint

--- src/lib.rs
+++ src/lib.rs
@@ -340,6 +340,36 @@ fn read_file(comp_path: PathBuf) -> Option<String> {
     Some(comp_lines)
 }
 
+// pub fn flush_read_file_cache() {
+//     memoized_flush_read_file()
+// }
+
+/// Returns the percentage of lines from `ref_lines` that also exist in `comp_lines`.
+///
+///
+/// # Examples
+///
+/// ```
+/// //                ✓   ✓  x   ✓   x      = 3
+/// let ref_lines = "12\n14\n5\n17\n19\n";
+/// let comp_lines = "11\n12\n13\n14\n15\n16\n\n17\n18\n";
+/// let result = busca::get_percent_matching_lines(ref_lines, comp_lines);
+/// assert_eq!(result, 3.0 / 7.0);
+/// ```
+/// ---
+/// ```
+/// //                ✓   ✓  x   x    = 2 / 4 = 0.5
+/// let ref_lines = "12\n14\n5\n17";
+/// let comp_lines = "11\n12\n13\n14\n15\n16\n\n17\n18\n";
+/// let result = busca::get_percent_matching_lines(ref_lines, comp_lines);
+/// assert_eq!(result, 4.0 / 13.0);
+/// ```
+///
+pub fn get_percent_matching_lines(ref_lines: &str, comp_lines: &str) -> f32 {
+    let diff = TextDiff::from_lines(ref_lines, comp_lines);
+    diff.ratio()
+}
+
 #[cfg(test)]
 mod test_compare_file {
     use super::*;
@@ -470,30 +500,4 @@ mod test_compare_file {
 
         assert_eq!(file_comparison, None);
     }
-}
-
-/// Returns the percentage of lines from `ref_lines` that also exist in `comp_lines`.
-///
-///
-/// # Examples
-///
-/// ```
-/// //                ✓   ✓  x   ✓   x      = 3
-/// let ref_lines = "12\n14\n5\n17\n19\n";
-/// let comp_lines = "11\n12\n13\n14\n15\n16\n\n17\n18\n";
-/// let result = busca::get_percent_matching_lines(ref_lines, comp_lines);
-/// assert_eq!(result, 3.0 / 7.0);
-/// ```
-/// ---
-/// ```
-/// //                ✓   ✓  x   x    = 2 / 4 = 0.5
-/// let ref_lines = "12\n14\n5\n17";
-/// let comp_lines = "11\n12\n13\n14\n15\n16\n\n17\n18\n";
-/// let result = busca::get_percent_matching_lines(ref_lines, comp_lines);
-/// assert_eq!(result, 4.0 / 13.0);
-/// ```
-///
-pub fn get_percent_matching_lines(ref_lines: &str, comp_lines: &str) -> f32 {
-    let diff = TextDiff::from_lines(ref_lines, comp_lines);
-    diff.ratio()
 }
